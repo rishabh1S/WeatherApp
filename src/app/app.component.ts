40d2dc7bc3bb68cd3d0f1a0c482a3de6b76215de
Rishabh Singh <rishabh15112001@gmail.com>
2023-05-04 19:22:57 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'myapp';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+  currentTemp?: number;
+  realFeelTemp?: number;
+  Pressure?: number;
+  isDay?: number;
+  location?: string;
+  windSpeed?: number;
+  humidity?: number;
+  condition?: string;
+  weatherData?: WeatherData;
+  cityName: string = 'Bhopal';
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        this.location = this.weatherData.location.name;
+        this.condition = this.weatherData.current.condition.text;
+        this.currentTemp = this.weatherData.current.temp_c;
+        this.realFeelTemp = this.weatherData.current.feelslike_c;
+        this.isDay = this.weatherData.current.is_day;
+        this.Pressure = this.weatherData.current.pressure_mb;
+        this.windSpeed = this.weatherData.current.wind_kph;
+        this.humidity = this.weatherData.current.humidity;
+        console.log(this.humidity);
+      },
+    });
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment: any = {
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '63252d8a04msha74a33777e6f4b8p1cdf31jsn33efd87e4763',
+};
